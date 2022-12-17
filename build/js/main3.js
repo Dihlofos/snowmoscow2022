9e1bdf3cfdacb3888dfe9189b77ac0012b4f518a
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2022-12-17 10:11:32 +0700

    Done all desktop markup without animation

--- build/js/main3.js
+++ build/js/main3.js
@@ -1,5 +1,24 @@
 "use strict";
 (function () {
+  let upButton = document.querySelector(".up");
+
+  if (upButton) {
+    window.onscroll = function () {
+      if (window.pageYOffset > 260) {
+        upButton.classList.add("up--shown");
+      } else {
+        upButton.classList.remove("up--shown");
+      }
+    };
+
+    upButton.onclick = function () {
+      window.scrollTo(0, 0);
+    };
+  }
+})();
+
+"use strict";
+(function () {
   const data = {
     11: [
       {
